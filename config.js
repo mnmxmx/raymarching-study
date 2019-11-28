e8eb7e46391db68a2749f0c8c37a25b500e918d5
Misaki <nknmsm1992@gmail.com>
2019-11-28 11:30:19 +0900

    changed directory name

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-module.exports = {"files":["01_distortion","02_boolean","03_repetition","04_metaball","05_cubes","06_voxel","07_voxel2"]}
+module.exports = {"files":["01_distortion","02_boolean","03_repetition","04_metaball","05_cubes","06_voxel","07_voxel_ao"]}
